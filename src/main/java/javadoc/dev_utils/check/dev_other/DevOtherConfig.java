6e0a0c0f1341f220a85e017fb266ceb126b1c1f8
Ttt <13798405957@163.com>
2019-09-10 02:40:23 +0800

    1.更新 DevOther 代码检测忽略配置处理

--- src/main/java/javadoc/dev_utils/check/dev_other/DevOtherConfig.java
+++ src/main/java/javadoc/dev_utils/check/dev_other/DevOtherConfig.java
@@ -56,16 +56,26 @@ final class DevOtherConfig {
      * 初始化 忽略指定方法注释处理
      */
     private static void initIgnoreAnnotateMap() {
+        sIgnoreAnnotateMap.put("AccessibilityListenerService", Utils.asList(
+                "onCreate","onDestroy"));
         sIgnoreAnnotateMap.put("GlideTransformUtils.GlideBlurformation", Utils.asList(
-                "transform", "updateDiskCacheKey"));
+                "transform","updateDiskCacheKey"));
         sIgnoreAnnotateMap.put("GlideTransformUtils.GlideCircleTransform", Utils.asList(
-                "transform", "circleCrop", "updateDiskCacheKey"));
+                "transform","circleCrop","updateDiskCacheKey"));
         sIgnoreAnnotateMap.put("GlideTransformUtils.GlideRoundTransform", Utils.asList(
-                "transform", "roundCrop", "updateDiskCacheKey"));
+                "transform","roundCrop","updateDiskCacheKey"));
         sIgnoreAnnotateMap.put("GlideTransformUtils.RotateTransformation", Utils.asList(
-                "transform", "updateDiskCacheKey"));
+                "transform","updateDiskCacheKey"));
         sIgnoreAnnotateMap.put("GlideUtils", Utils.asList(
-                "with", "with", "with", "with", "with", "with"));
+                "with","with","with","with","with","with"));
+        sIgnoreAnnotateMap.put("NetWorkReceiver", Utils.asList("onReceive"));
+        sIgnoreAnnotateMap.put("NotificationService", Utils.asList(
+                "onCreate","onDestroy","onStartCommand"));
+        sIgnoreAnnotateMap.put("PhoneReceiver", Utils.asList("onReceive"));
+        sIgnoreAnnotateMap.put("ScreenReceiver", Utils.asList("onReceive"));
+        sIgnoreAnnotateMap.put("SmsReceiver", Utils.asList("onReceive"));
+        sIgnoreAnnotateMap.put("TimeReceiver", Utils.asList("onReceive"));
+        sIgnoreAnnotateMap.put("WifiReceiver", Utils.asList("onReceive"));
     }
 
     // =
@@ -74,16 +84,26 @@ final class DevOtherConfig {
      * 初始化 忽略指定方法参数 final 修饰符
      */
     private static void initIgnoreFinalMap() {
+        sIgnoreFinalMap.put("AccessibilityListenerService", Utils.asList(
+                "onAccessibilityEvent"));
         sIgnoreFinalMap.put("GlideTransformUtils.GlideBlurformation", Utils.asList(
-                "transform", "updateDiskCacheKey", "blurBitmap"));
+                "transform","updateDiskCacheKey","blurBitmap"));
         sIgnoreFinalMap.put("GlideTransformUtils.GlideCircleTransform", Utils.asList(
-                "transform", "circleCrop", "updateDiskCacheKey"));
+                "transform","circleCrop","updateDiskCacheKey"));
         sIgnoreFinalMap.put("GlideTransformUtils.GlideRoundTransform", Utils.asList(
-                "transform", "roundCrop", "updateDiskCacheKey"));
+                "transform","roundCrop","updateDiskCacheKey"));
         sIgnoreFinalMap.put("GlideTransformUtils.RotateTransformation", Utils.asList(
-                "transform", "updateDiskCacheKey"));
+                "transform","updateDiskCacheKey"));
         sIgnoreFinalMap.put("GlideUtils", Utils.asList(
-                "with", "with", "with", "with", "with", "with"));
+                "with","with","with","with","with","with"));
+        sIgnoreFinalMap.put("NetWorkReceiver", Utils.asList("onReceive"));
+        sIgnoreFinalMap.put("NotificationService", Utils.asList(
+                "onNotificationPosted","onNotificationRemoved","onStartCommand"));
+        sIgnoreFinalMap.put("PhoneReceiver", Utils.asList("onReceive"));
+        sIgnoreFinalMap.put("ScreenReceiver", Utils.asList("onReceive"));
+        sIgnoreFinalMap.put("SmsReceiver", Utils.asList("onReceive"));
+        sIgnoreFinalMap.put("TimeReceiver", Utils.asList("onReceive"));
+        sIgnoreFinalMap.put("WifiReceiver", Utils.asList("onReceive"));
     }
 
     // =
@@ -92,19 +112,29 @@ final class DevOtherConfig {
      * 初始化 忽略指定方法 static 修饰符
      */
     private static void initIgnoreStaticMap() {
+        sIgnoreStaticMap.put("AccessibilityListenerService", Utils.asList(
+                "onAccessibilityEvent","onInterrupt","onServiceConnected","onCreate","onDestroy"));
         sIgnoreStaticMap.put("GlideTransformUtils.GlideBlurformation", Utils.asList(
-                "transform", "updateDiskCacheKey", "blurBitmap"));
+                "transform","updateDiskCacheKey","blurBitmap"));
         sIgnoreStaticMap.put("GlideTransformUtils.GlideCircleTransform", Utils.asList(
-                "transform", "circleCrop", "updateDiskCacheKey"));
+                "transform","circleCrop","updateDiskCacheKey"));
         sIgnoreStaticMap.put("GlideTransformUtils.GlideRoundTransform", Utils.asList(
-                "transform", "roundCrop", "updateDiskCacheKey"));
+                "transform","roundCrop","updateDiskCacheKey"));
         sIgnoreStaticMap.put("GlideTransformUtils.RotateTransformation", Utils.asList(
-                "transform", "updateDiskCacheKey"));
+                "transform","updateDiskCacheKey"));
         sIgnoreStaticMap.put("GlideUtils.GlideLoader", Utils.asList(
-                "preload", "preload", "displayImage", "displayImage", "displayImageToGif",
-                "displayImageToGif", "loadImageBitmap", "loadImageBitmap", "loadImageDrawable",
-                "loadImageDrawable", "loadImageFile", "loadImageFile", "loadImageGif", "loadImageGif",
-                "cancelDisplayTask", "cancelDisplayTask", "destroy", "pause", "resume", "stop", "start"));
+                "preload","preload","displayImage","displayImage","displayImageToGif",
+                "displayImageToGif","loadImageBitmap","loadImageBitmap","loadImageDrawable",
+                "loadImageDrawable","loadImageFile","loadImageFile","loadImageGif","loadImageGif",
+                "cancelDisplayTask","cancelDisplayTask","destroy","pause","resume","stop","start"));
+        sIgnoreStaticMap.put("NetWorkReceiver", Utils.asList("onReceive"));
+        sIgnoreStaticMap.put("NotificationService", Utils.asList(
+                "onNotificationPosted","onNotificationRemoved","onCreate","onDestroy","onStartCommand","cancelNotification"));
+        sIgnoreStaticMap.put("PhoneReceiver", Utils.asList("onReceive"));
+        sIgnoreStaticMap.put("ScreenReceiver", Utils.asList("onReceive"));
+        sIgnoreStaticMap.put("SmsReceiver", Utils.asList("onReceive"));
+        sIgnoreStaticMap.put("TimeReceiver", Utils.asList("onReceive"));
+        sIgnoreStaticMap.put("WifiReceiver", Utils.asList("onReceive"));
     }
 
     /**
@@ -112,14 +142,22 @@ final class DevOtherConfig {
      */
     private static void initIgnoreParamMap() {
         sIgnoreParamMap.put("GlideTransformUtils.GlideBlurformation", Utils.asList(
-                "transform", "updateDiskCacheKey", "blurBitmap"));
+                "transform","updateDiskCacheKey","blurBitmap"));
         sIgnoreParamMap.put("GlideTransformUtils.GlideCircleTransform", Utils.asList(
-                "transform", "circleCrop", "updateDiskCacheKey"));
+                "transform","circleCrop","updateDiskCacheKey"));
         sIgnoreParamMap.put("GlideTransformUtils.GlideRoundTransform", Utils.asList(
-                "transform", "roundCrop", "updateDiskCacheKey"));
+                "transform","roundCrop","updateDiskCacheKey"));
         sIgnoreParamMap.put("GlideTransformUtils.RotateTransformation", Utils.asList(
-                "transform", "updateDiskCacheKey"));
-        sIgnoreParamMap.put("GlideUtils", Utils.asList("with", "with", "with", "with", "with", "with"));
+                "transform","updateDiskCacheKey"));
+        sIgnoreParamMap.put("GlideUtils", Utils.asList(
+                "with","with","with","with","with","with"));
+        sIgnoreParamMap.put("NetWorkReceiver", Utils.asList("onReceive"));
+        sIgnoreParamMap.put("NotificationService", Utils.asList("onStartCommand"));
+        sIgnoreParamMap.put("PhoneReceiver", Utils.asList("onReceive"));
+        sIgnoreParamMap.put("ScreenReceiver", Utils.asList("onReceive"));
+        sIgnoreParamMap.put("SmsReceiver", Utils.asList("onReceive"));
+        sIgnoreParamMap.put("TimeReceiver", Utils.asList("onReceive"));
+        sIgnoreParamMap.put("WifiReceiver", Utils.asList("onReceive"));
     }
 
     /**
@@ -132,14 +170,27 @@ final class DevOtherConfig {
      * 初始化 忽略指定方法非 public 处理
      */
     private static void initIgnoreUnPublicMap() {
+        sIgnoreUnPublicMap.put("AccessibilityListenerService", Utils.asList(
+                "onServiceConnected"));
         sIgnoreUnPublicMap.put("GlideTransformUtils.GlideBlurformation", Utils.asList(
                 "transform"));
         sIgnoreUnPublicMap.put("GlideTransformUtils.GlideCircleTransform", Utils.asList(
-                "transform", "circleCrop"));
+                "transform","circleCrop"));
         sIgnoreUnPublicMap.put("GlideTransformUtils.GlideRoundTransform", Utils.asList(
-                "transform", "roundCrop"));
+                "transform","roundCrop"));
         sIgnoreUnPublicMap.put("GlideTransformUtils.RotateTransformation", Utils.asList(
                 "transform"));
+        sIgnoreUnPublicMap.put("NetWorkReceiver.NetwordStateListener", Utils.asList(
+                "onNetworkState"));
+        sIgnoreUnPublicMap.put("NotificationService.NotificationListener", Utils.asList(
+                "onServiceCreated","onServiceDestroy","onStartCommand",
+                "onNotificationPosted","onNotificationRemoved"));
+        sIgnoreUnPublicMap.put("PhoneReceiver.PhoneListener", Utils.asList(
+                "onPhoneStateChanged"));
+        sIgnoreUnPublicMap.put("ScreenReceiver.ScreenListener", Utils.asList(
+                "screenOn","screenOff","userPresent"));
+        sIgnoreUnPublicMap.put("TimeReceiver.TimeListener", Utils.asList(
+                "onTimeZoneChanged","onTimeChanged","onTimeTick"));
     }
 
     // =
