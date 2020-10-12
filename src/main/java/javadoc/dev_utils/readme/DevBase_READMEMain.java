f7bf5c9be47f2a3af8abe961e710b69d9aaaf57f
Ttt <13798405957@163.com>
2020-10-12 12:22:43 +0800

    1.更新部分 DevBase 生成 README 代码

--- src/main/java/javadoc/dev_utils/readme/DevBase_READMEMain.java
+++ src/main/java/javadoc/dev_utils/readme/DevBase_READMEMain.java
@@ -44,27 +44,30 @@ final class DevBase_READMEMain {
         buffer.append(PackageCatalog.apiCatalog(false, path, packageName, mapCatelog));
 
         buffer.append("\n\n");
-        buffer.append("### 项目类结构 - [包目录](" + githubUrl + ")");
+        buffer.append("## 项目类结构 - [包目录](" + githubUrl + ")");
         buffer.append("\n\n");
-        buffer.append("#### 核心代码");
+        buffer.append("### 核心代码");
         buffer.append("\n\n");
         buffer.append("* 核心 Base Activity（[AbstractDevBaseActivity](" + githubUrl + "/activity)）：整个库 Activity 基类都基于该模块代码");
         buffer.append("\n\n");
         buffer.append("* 核心 Base Fragment（[AbstractDevBaseFragment](" + githubUrl + "/fragment)）：整个库 Fragment 基类都基于该模块代码");
         buffer.append("\n\n");
-        buffer.append("#### 其他代码");
+        buffer.append("### 其他代码");
         buffer.append("\n\n");
         buffer.append("* 接口相关（[able](" + githubUrl + "/able)）：对外提供开放方法接口，服务基类用于可选配置及获取操作");
         buffer.append("\n\n");
-        buffer.append("* 库依赖工具包（[utils](" + githubUrl + "/utils)）：抽取通用代码工具类、封装基类相同逻辑代码辅助类");
+        buffer.append("* 库依赖工具包（[utils](" + githubUrl + "/utils)）：抽取通用代码工具类、封装相同逻辑代码辅助类");
         buffer.append("\n\n");
-        buffer.append("#### 基于 Base Activity、Fragment 扩展包 - [expand](" + githubUrl + "/expand)");
+        buffer.append("### 基于 Base Activity、Fragment 扩展包（[expand](" + githubUrl + "/expand)）");
         buffer.append("\n\n");
-        buffer.append("* 内置 XML Layout 基类（[content](" + githubUrl + "/expand/content)）：对外提供开放方法接口，服务基类用于可选配置及获取操作");
+        buffer.append("* 内置 XML Layout 基类（[content](" + githubUrl + "/expand/content)）：通过内置 Layout 作为根布局，方便对全局进行增删 View 控制处理");
         buffer.append("\n\n");
-        buffer.append("* MVP 架构基类（[mvp](" + githubUrl + "/expand/mvp)）：抽取通用代码工具类、封装基类相同逻辑代码辅助类");
+        buffer.append("* MVP 架构基类（[mvp](" + githubUrl + "/expand/mvp)）：MVP Contract 架构封装基类");
         buffer.append("\n\n");
-        buffer.append("* ViewBinding 基类（[mvp](" + githubUrl + "/expand/viewbinding)）：抽取通用代码工具类、封装基类相同逻辑代码辅助类");
+        buffer.append("* ViewBinding 基类（[viewbinding](" + githubUrl + "/expand/viewbinding)）：使用 ViewBinding 实现对 View 进行 bind 基类");
+
+        buffer.append("\n\n");
+        buffer.append("## 设计思路");
         buffer.append("\n\n");
 
         buffer.append("\n\n");
