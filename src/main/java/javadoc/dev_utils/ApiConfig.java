ebf12abd30191152e678bb6fca469f9d8bcab65a
Cjt <jtongttt@gmail.com>
2021-06-04 01:55:50 +0800

    1.更新 DevAssist 版本

--- src/main/java/javadoc/dev_utils/ApiConfig.java
+++ src/main/java/javadoc/dev_utils/ApiConfig.java
@@ -74,7 +74,7 @@ public final class ApiConfig {
     // DevAssist GitHub 项目文件路径
     public static final String DEV_ASSIST_GITHUB_URL         = GITHUB_AUTHOR_URL + "/DevUtils/blob/master/lib/DevAssist/src/main/java/dev";
     // DevAssist 版本号
-    public static final String DEV_ASSIST_VERSION            = "1.2.2";
+    public static final String DEV_ASSIST_VERSION            = "1.2.3";
 
     // ============
     // = DevOther =
