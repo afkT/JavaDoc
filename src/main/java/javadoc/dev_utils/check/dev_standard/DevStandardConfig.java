b8ec0f71c83870be1d78b9f1a5c77088cba5b1de
Cjt <jtongttt@gmail.com>
2021-12-20 11:38:24 +0800

    1.更新部分版本信息、忽略配置

--- src/main/java/javadoc/dev_utils/check/dev_standard/DevStandardConfig.java
+++ src/main/java/javadoc/dev_utils/check/dev_standard/DevStandardConfig.java
@@ -28,7 +28,7 @@ final class DevStandardConfig implements ICheckConfig {
         ignoreAnnotateMap.put("Main", ArrayUtils.asListArgs("print","main"));
         ignoreAnnotateMap.put("PanguAnalyzeMain", ArrayUtils.asListArgs("main"));
         ignoreAnnotateMap.put("SortMain", ArrayUtils.asListArgs("main"));
-        ignoreAnnotateMap.put("StateMain", ArrayUtils.asListArgs("main"));
+        ignoreAnnotateMap.put("StateGenerateMain", ArrayUtils.asListArgs("main"));
 
         return ignoreAnnotateMap;
     }
@@ -49,7 +49,7 @@ final class DevStandardConfig implements ICheckConfig {
         ignoreFinalMap.put("Pangu", ArrayUtils.asListArgs("spacingText","spacingFile"));
         ignoreFinalMap.put("PanguAnalyzeMain", ArrayUtils.asListArgs("main"));
         ignoreFinalMap.put("SortMain", ArrayUtils.asListArgs("main"));
-        ignoreFinalMap.put("StateMain", ArrayUtils.asListArgs("main"));
+        ignoreFinalMap.put("StateGenerateMain", ArrayUtils.asListArgs("main"));
 
         return ignoreFinalMap;
     }
@@ -84,7 +84,7 @@ final class DevStandardConfig implements ICheckConfig {
         ignoreParamMap.put("Main", ArrayUtils.asListArgs("print","main"));
         ignoreParamMap.put("PanguAnalyzeMain", ArrayUtils.asListArgs("main"));
         ignoreParamMap.put("SortMain", ArrayUtils.asListArgs("main"));
-        ignoreParamMap.put("StateMain", ArrayUtils.asListArgs("main"));
+        ignoreParamMap.put("StateGenerateMain", ArrayUtils.asListArgs("main"));
 
         return ignoreParamMap;
     }
